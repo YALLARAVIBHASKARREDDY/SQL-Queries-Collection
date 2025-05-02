333da0c5075af479a3e65c75d892f57bef6a2776
RAVI BHASKAR REDDY <200175449+YALLARAVIBHASKARREDDY@users.noreply.github.com>
2025-05-02 10:28:01 +0530

    Update row_number-example.sql

--- WindowFunctions/row_number-example.sql
+++ WindowFunctions/row_number-example.sql
@@ -1,1 +1,9 @@
 
+-- ROW_NUMBER(): Assigns a unique number to each row within a partition
+
+SELECT 
+    EmployeeID,
+    EmployeeName,
+    DepartmentID,
+    ROW_NUMBER() OVER (PARTITION BY DepartmentID ORDER BY EmployeeName) AS RowNum
+FROM Employees;
