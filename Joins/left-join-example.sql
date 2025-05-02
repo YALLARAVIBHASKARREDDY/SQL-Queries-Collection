ed6704de85ddd2b8a4462265ac64ee024608241c
RAVI BHASKAR REDDY <200175449+YALLARAVIBHASKARREDDY@users.noreply.github.com>
2025-05-02 10:26:46 +0530

    Update left-join-example.sql

--- Joins/left-join-example.sql
+++ Joins/left-join-example.sql
@@ -1,1 +1,6 @@
 
+-- LEFT JOIN: Returns all records from the left table, and matched records from the right table
+
+SELECT c.CustomerName, o.OrderID
+FROM Customers c
+LEFT JOIN Orders o ON c.CustomerID = o.CustomerID;
