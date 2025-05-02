d6eecb0c953d5e24df5556c7ca627890bc0a27c8
RAVI BHASKAR REDDY <200175449+YALLARAVIBHASKARREDDY@users.noreply.github.com>
2025-05-02 10:28:26 +0530

    Update rank-example.sql

--- WindowFunctions/rank-example.sql
+++ WindowFunctions/rank-example.sql
@@ -1,1 +1,8 @@
 
+-- RANK(): Assigns a rank to each row within a partition of a result set
+
+SELECT 
+    EmployeeID,
+    Salary,
+    RANK() OVER (ORDER BY Salary DESC) AS SalaryRank
+FROM Employees;
