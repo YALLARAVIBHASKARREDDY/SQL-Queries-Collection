57a463f79aff342ca644b258bf5d5fe0bd6bd3a1
RAVI BHASKAR REDDY <200175449+YALLARAVIBHASKARREDDY@users.noreply.github.com>
2025-05-02 10:27:32 +0530

    Update nested-subquery.sql

--- Subqueries/nested-subquery.sql
+++ Subqueries/nested-subquery.sql
@@ -1,1 +1,10 @@
 
+-- Nested Subquery: Subquery inside another subquery
+
+SELECT EmployeeName
+FROM Employees
+WHERE EmployeeID IN (
+    SELECT ManagerID
+    FROM Departments
+    WHERE LocationID = 100
+);
