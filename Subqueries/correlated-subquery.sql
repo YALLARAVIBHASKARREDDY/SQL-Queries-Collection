33f496b314af93679dbcce5d2bee9defcb7e262d
RAVI BHASKAR REDDY <200175449+YALLARAVIBHASKARREDDY@users.noreply.github.com>
2025-05-02 10:27:17 +0530

    Update correlated-subquery.sql

--- Subqueries/correlated-subquery.sql
+++ Subqueries/correlated-subquery.sql
@@ -1,1 +1,10 @@
 
+-- Correlated Subquery: Subquery depends on the outer query
+
+SELECT e.EmployeeName
+FROM Employees e
+WHERE Salary > (
+    SELECT AVG(Salary)
+    FROM Employees
+    WHERE DepartmentID = e.DepartmentID
+);
