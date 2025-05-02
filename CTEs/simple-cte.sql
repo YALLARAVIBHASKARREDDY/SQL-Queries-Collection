2894bff54726d42cae0f744f2e181304dda5ff07
RAVI BHASKAR REDDY <200175449+YALLARAVIBHASKARREDDY@users.noreply.github.com>
2025-05-02 10:29:41 +0530

    Update simple-cte.sql

--- CTEs/simple-cte.sql
+++ CTEs/simple-cte.sql
@@ -1,1 +1,9 @@
 
+-- Simple CTE: Common Table Expression to simplify a query
+
+WITH EmployeeCTE AS (
+    SELECT EmployeeID, EmployeeName, Salary
+    FROM Employees
+    WHERE Salary > 50000
+)
+SELECT * FROM EmployeeCTE;
