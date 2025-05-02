602c9b148f3264b8bb4c6e46d7b583985c486726
RAVI BHASKAR REDDY <200175449+YALLARAVIBHASKARREDDY@users.noreply.github.com>
2025-05-02 10:29:13 +0530

    Update recursive-cte-example.sql

--- CTEs/recursive-cte-example.sql
+++ CTEs/recursive-cte-example.sql
@@ -1,1 +1,8 @@
+-- Recursive CTE: Example to generate a series of numbers
 
+WITH Numbers AS (
+    SELECT 1 AS Number
+    UNION ALL
+    SELECT Number + 1 FROM Numbers WHERE Number < 10
+)
+SELECT * FROM Numbers;
